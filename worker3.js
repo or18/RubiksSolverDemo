ef519bf9ed184ff24ff51539b037dbfaadb968eb
or18 <119738717+or18@users.noreply.github.com>
2024-10-06 19:48:41 +0900

    Add files via upload

---
+++ worker3.js
@@ -0,0 +1,8 @@
+importScripts('pseudo.js');
+
+self.onmessage = function(event){
+	const {scr, rot, slot, pslot, num, len, restrict} = event.data;
+	Module.onRuntimeInitialized = function(){
+		Module.solve(scr, rot, slot, pslot, num, len, restrict);
+	};
+};
