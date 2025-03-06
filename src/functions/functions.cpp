702561e24e3df7f0db559b2c08c6ec072b7fe454
or18 <nozabigya@gmail.com>
2025-03-06 23:20:36 +0900

    update index.html

--- src/functions/functions.cpp
+++ src/functions/functions.cpp
@@ -460,9 +460,134 @@ std::string ConvertScramble(std::string str)
 	return result2;
 }
 
+struct State
+{
+	std::vector<int> sc;
+
+	State(std::vector<int> arg_sc = {0, 1, 2, 3, 4, 5, 6, 7, 8,
+									 9, 10, 11, 12, 13, 14, 15, 16, 17,
+									 18, 19, 20, 21, 22, 23, 24, 25, 26,
+									 27, 28, 29, 30, 31, 32, 33, 34, 35,
+									 36, 37, 38, 39, 40, 41, 42, 43, 44,
+									 45, 46, 47, 48, 49, 50, 51, 52, 53}) : sc(arg_sc) {}
+
+	State apply_move(State move)
+	{
+		std::vector<int> new_sc;
+		for (int i = 0; i < 54; ++i)
+		{
+			int p = move.sc[i];
+			new_sc.emplace_back(sc[p]);
+		}
+		return State(new_sc);
+	}
+};
+
+std::string StateToInput(State state)
+{
+	std::vector<int> sc = state.sc;
+	std::string input = "";
+	for (int i : sc)
+	{
+		if (i < 9)
+		{
+			input += "U";
+		}
+		else if (i < 18)
+		{
+			input += "R";
+		}
+		else if (i < 27)
+		{
+			input += "F";
+		}
+		else if (i < 36)
+		{
+			input += "D";
+		}
+		else if (i < 45)
+		{
+			input += "L";
+		}
+		else
+		{
+			input += "B";
+		}
+	}
+	return input;
+}
+
+std::unordered_map<std::string, State> moves = {
+	{"U", State({6, 3, 0, 7, 4, 1, 8, 5, 2,
+				 45, 46, 47, 12, 13, 14, 15, 16, 17,
+				 9, 10, 11, 21, 22, 23, 24, 25, 26,
+				 27, 28, 29, 30, 31, 32, 33, 34, 35,
+				 18, 19, 20, 39, 40, 41, 42, 43, 44,
+				 36, 37, 38, 48, 49, 50, 51, 52, 53})},
+	{"R", State({0, 1, 20, 3, 4, 23, 6, 7, 26,
+				 15, 12, 9, 16, 13, 10, 17, 14, 11,
+				 18, 19, 29, 21, 22, 32, 24, 25, 35,
+				 27, 28, 51, 30, 31, 48, 33, 34, 45,
+				 36, 37, 38, 39, 40, 41, 42, 43, 44,
+				 8, 46, 47, 5, 49, 50, 2, 52, 53})},
+	{"F", State({0, 1, 2, 3, 4, 5, 44, 41, 38,
+				 6, 10, 11, 7, 13, 14, 8, 16, 17,
+				 24, 21, 18, 25, 22, 19, 26, 23, 20,
+				 15, 12, 9, 30, 31, 32, 33, 34, 35,
+				 36, 37, 27, 39, 40, 28, 42, 43, 29,
+				 45, 46, 47, 48, 49, 50, 51, 52, 53})},
+	{"D", State({0, 1, 2, 3, 4, 5, 6, 7, 8,
+				 9, 10, 11, 12, 13, 14, 24, 25, 26,
+				 18, 19, 20, 21, 22, 23, 42, 43, 44,
+				 33, 30, 27, 34, 31, 28, 35, 32, 29,
+				 36, 37, 38, 39, 40, 41, 51, 52, 53,
+				 45, 46, 47, 48, 49, 50, 15, 16, 17})},
+	{"L", State({53, 1, 2, 50, 4, 5, 47, 7, 8,
+				 9, 10, 11, 12, 13, 14, 15, 16, 17,
+				 0, 19, 20, 3, 22, 23, 6, 25, 26,
+				 18, 28, 29, 21, 31, 32, 24, 34, 35,
+				 42, 39, 36, 43, 40, 37, 44, 41, 38,
+				 45, 46, 33, 48, 49, 30, 51, 52, 27})},
+	{"B", State({11, 14, 17, 3, 4, 5, 6, 7, 8,
+				 9, 10, 35, 12, 13, 34, 15, 16, 33,
+				 18, 19, 20, 21, 22, 23, 24, 25, 26,
+				 27, 28, 29, 30, 31, 32, 36, 39, 42,
+				 2, 37, 38, 1, 40, 41, 0, 43, 44,
+				 51, 48, 45, 52, 49, 46, 53, 50, 47})}};
+
+std::string ScrambleToState(std::string scramble){
+		std::vector<std::string> move_names2;
+		std::vector<std::string> faces;
+		for (const auto& pair : moves) {
+			faces.push_back(pair.first);
+		}
+		for (const auto& face_name : faces) {
+			move_names2.push_back(face_name);
+			move_names2.push_back(face_name + "2");
+			move_names2.push_back(face_name + "'");
+			moves[face_name + "2"] = moves[face_name].apply_move(moves[face_name]);
+			moves[face_name + "'"] = moves[face_name + "2"].apply_move(moves[face_name]);
+		}
+
+		State state = State();
+		std::string name;
+		std::istringstream iss(scramble);
+		while (iss >> name)
+		{
+			if (!name.empty())
+			{
+				state = state.apply_move(moves[name]);
+			}
+		}
+		std::string output = StateToInput(state);
+		return output;
+}
+
 EMSCRIPTEN_BINDINGS(my_module)
 {
 	emscripten::function("scr_mirror", &MirrorScramble);
 	emscripten::function("scr_reverse", &ReverseScramble);
 	emscripten::function("scr_converter", &ConvertScramble);
+	emscripten::function("ScrambleToState", &ScrambleToState);
+
 }
