f4d11d707b120cf5ee2a6926da107a75148fb9ae
or18 <nozabigya@gmail.com>
2025-09-22 08:24:43 +0900

    highMemorySolver bug fix

--- src/highMemorySolver/worker2.js
+++ src/highMemorySolver/worker2.js
@@ -1,7 +1,7 @@
 importScripts('solver2.js');
 
 self.onmessage = function (event) {
-	const { solver, scr, rot, slot, ll, num, len, move_restrict, post_alg, center_offset, max_rot_count, ma, mcString } = event.data;
+	const { solver, scr, rot, slot, ll, num, len, move_restrict, post_alg, center_offset, max_rot_count, ma2, mcString } = event.data;
 	Module.onRuntimeInitialized = function () {
 		try {
 			Module.solve(solver, scr, rot, slot, ll, num, len, move_restrict, post_alg, center_offset, max_rot_count, ma2, mcString);
